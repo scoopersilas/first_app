73f4f424709c07fede7ba436a693afe1ddff33bc
Shana Silas <chefshana@me.com>
2013-10-07 20:19:43 -0400

    Inital Repoository for Ruby on Rails Tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '03555938b1e99431364b029235a8c75f747b41a21d8a73d3c8404f2a0c0452fe55a8fd4ed45c5ed42e413a424375b96f65fc991b96aafede75ae65521d78c220'
